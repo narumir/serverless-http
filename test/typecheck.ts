3201a4c1b3a84fc36ed258f171a824624f890804
narumir <openjacoblee@gmail.com>
2020-03-21 23:05:14 +0900

    remove example and test code

--- test/typecheck.ts
+++
@@ -1,6 +0,0 @@
-import Koa = require("koa");
-import serverlessHttp = require("..");
-
-// Basic definitions check.
-const handlerWithFn: serverlessHttp.Handler = serverlessHttp(() => { });
-const handlerWithApp: serverlessHttp.Handler = serverlessHttp(new Koa());
